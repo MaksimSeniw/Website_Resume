cda6822f09696eb41b4c9fbcc82c2785378104e9
MaksimSeniw <mase5436@colorado.edu>
2023-09-14 13:17:46 -0600

    fixed website repo link

--- Resume_Website/data/profile.js
+++ Resume_Website/data/profile.js
@@ -15,7 +15,7 @@ const profileData = {
     { title: "Resume", src: "https://shorturl.at/lnGO5" },
     { title: "Github", src: "https://github.com/MaksimSeniw" },
     { title: "LinkedIn", src: "https://www.linkedin.com/in/maks-seniw" },
-    { title: "Repo for This Page", src: "https://shorturl.at/adEUY" },
+    { title: "Repo for This Page", src: "https://github.com/MaksimSeniw/Website_Resume" },
   ],
   education: [
     {
