8b66083c09819ef84282c2d2dd4a1c1641a4b848
MaksimSeniw <114446772+MaksimSeniw@users.noreply.github.com>
2026-01-03 13:26:08 -0700

    work update

--- V2/app.js
+++ V2/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, './views/static')));
 
 
 app.get('/', function(req, res) {
